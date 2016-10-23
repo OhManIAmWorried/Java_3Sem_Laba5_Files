e5a108620e1c32a25c6ba80c87f6bb8c2b4f536e
ManIAmWorried <wladimirr04@gmail.com>
2016-10-23 17:08:47 +0300

    [2] input and output

--- src/Laba5_Files.java
+++ src/Laba5_Files.java
@@ -1,9 +1,125 @@
+import java.io.*;
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.util.Date;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
-/**
- * Created by Oly on 23.10.2016.
- */
+public class Laba5_Files {
+    private static Scanner ascanner = new Scanner(System.in);
+    private static final String dir = "tours.txt";
+    private static final DateFormat adateform = new SimpleDateFormat("dd.MM.yyyy");
+    private static boolean swf = false;
+    public static void main(String[] args){
+        boolean sw = true;
+        Byte cv = 0;
+
+        while (sw){
+            System.out.println("0. Exit \n1. Show All Tours\n2. Add the Tour\n3. Delete the Tour\n4. Edit the Tour");
+            System.out.print("Choice: ");
+            cv = ascanner.nextByte();
+            switch (cv) {
+                case 0:{sw = false; break;}
+                case 1:{ fileOutput(); break;}
+                case 2:{ addTour(); break;}
+                case 3:{ deleteTour(); break;}
+                case 4:{ editTour(); break;}
+                default: {System.out.println("Error: BadInput"); break;}
+            }
+        }
+    }
+
+    private static String getString(Tour tmp){
+        return "Company: " + tmp.companyname
+                + "| to: " + tmp.country
+                + "| Departure: " + adateform.format(tmp.departuredate)
+                + "| Return: " + adateform.format(tmp.returndate)
+                + "| Amount: " + tmp.amount;
+    }
+
+    public static void fileOutput() {
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(dir));
+            Tour tmp;
+            int i = 0;
+            while ((tmp = (Tour) ois.readObject()) != null) {
+                System.out.println("[" + ++i + "] " + getString(tmp));
+            }
+            ois.close();
+        } catch (EOFException e) {
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void addTour(){
+        try {
+            ObjectOutputStream oos;
+            File f = new File(dir);
+            if ((!f.exists()) || (swf == false)){
+                oos = new ObjectOutputStream(new FileOutputStream(dir,true));
+            }
+            else{
+                oos = new AppendingObjectOutputStream(new FileOutputStream(dir,true));
+            }
+            Tour t = new Tour();
+            System.out.print("Company name: ");
+            t.companyname = ascanner.next();
+            System.out.print("Country: ");
+            t.country = ascanner.next();
+            System.out.print("Departure date [dd.MM.yyyy]: ");
+            try{t.departuredate = adateform.parse(ascanner.next());}catch(ParseException e){e.printStackTrace();}
+            System.out.print("Return date: [dd.MM.yyyy]: ");
+            try{t.returndate = adateform.parse(ascanner.next());}catch(ParseException e){e.printStackTrace();}
+            System.out.print("Amount of vouchers: ");
+            t.amount = ascanner.nextInt();
+            t.enable();
+            oos.writeObject(t);
+            oos.flush();
+            oos.close();
+            swf = true;
+        }
+        catch (FileNotFoundException e) {e.printStackTrace();}
+        catch (IOException e) {e.printStackTrace();}
+    }
+
+    public static void deleteTour(){
+        System.out.print("Tour index: "); int i = ascanner.nextInt();
+    }
+
+    public static void editTour(){
+
+    }
+}
+
+class Tour implements Serializable{
+    public String companyname;
+    public String country;
+    public Date departuredate;
+    public Date returndate;
+    public Integer amount;
+    private Boolean enabled;
+    public void enable(){this.enabled = true;}
+    public void disable(){this.enabled = false;}
+    public Boolean getEnabled(){return this.enabled;}
+}
+
+class AppendingObjectOutputStream extends ObjectOutputStream
+{
+    public AppendingObjectOutputStream(OutputStream out) throws IOException
+    {
+        super(out);
+    }
+    @Override
+    protected void writeStreamHeader() throws IOException
+    {
+        reset();
+    }
+}
 
 /*
 Контрольные вопросы
@@ -48,54 +164,3 @@ import java.util.Scanner;
   (при удалении - НЕ НАДО делать сдвиг всех последующих элементов - просто помечайте элемент в файле как "удаленный"
   (в нумерации записей при этом появятся "дырки") - кстати, именно так работают с файлами реальные СУБД).
 */
-
-public class Laba5_Files {
-  private static Scanner ascanner = new Scanner(System.in);
-  public static void main(String[] args){
-      boolean sw = true;
-      Byte cv = 0;
-      while (sw){
-          System.out.println("0. Exit");
-          System.out.println("1. Show All Tours");
-          System.out.println("2. Add the Tour");
-          System.out.println("3. Delete the Tour");
-          System.out.println("4. Edit the Tour");
-          System.out.print("Choice: ");
-          cv = ascanner.nextByte();
-          switch (cv) {
-              case 0:{sw = false; break;}
-              case 1:{ FileOutput(); break;}
-              case 2:{break;}
-              case 3:{break;}
-              case 4:{break;}
-              default: {System.out.println("Error: BadInput"); break;}
-          }
-      }
-  }
-  public static void FileOutput(){
-
-  }
-  public static Tour getTour(Integer i){
-
-  }
-  public static void addTour(){
-
-  }
-  public static void deleteTour(){
-
-  }
-  public static void editTour(){
-
-  }
-}
-
-class Tour{
-    public String companyname;
-    public String country;
-    public Date departuredate;
-    public Date returndate;
-    public Integer amount;
-    private Boolean enabled;
-    public void setEnabled(Boolean b){this.enabled = b;}
-    public Boolean getEnabled(){return this.enabled;}
-}
